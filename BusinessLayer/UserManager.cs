2183c5e0f9e2c96314a120d3546920d82a12566f
Emre Patan <emrepatan91@gmail.com>
2019-01-30 09:56:05 +0300

    Update

--- BusinessLayer/UserManager.cs
+++ BusinessLayer/UserManager.cs
@@ -56,6 +56,5 @@ namespace BusinessLayer
                 return null;
             }
         }
-
     }
 }
---
+++ MyInstagram/Controllers/ProfileController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyInstagram.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public ActionResult ProfilePage()
+        {
+            UserManager um = new UserManager();
+            User user = um.FindUser(1);
+            return View(user);
+        }
+    }
+}
